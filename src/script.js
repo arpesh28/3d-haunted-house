cb4f6fc064a7e18c471dbef0ceed759d18a92537
arpesh28 <arpeshgadekar@gmail.com>
2023-06-20 11:41:57 +0530

    lights randomness added

--- src/script.js
+++ src/script.js
@@ -331,19 +331,19 @@ const tick = () => {
   const ghost1Angle = elapsedTime * 0.5;
   ghost1.position.x = Math.cos(ghost1Angle) * 4;
   ghost1.position.z = Math.sin(ghost1Angle) * 4;
-  ghost1.position.y = Math.cos(elapsedTime) * 3;
+  ghost1.position.y = Math.cos(elapsedTime) * 1.5;
 
   const ghost2Angle = -elapsedTime * 0.32;
   ghost2.position.x = Math.cos(ghost2Angle) * 5;
   ghost2.position.z = Math.sin(ghost2Angle) * 5;
-  ghost2.position.y = Math.cos(elapsedTime) * 4 + Math.cos(elapsedTime) * 2.5;
+  ghost2.position.y = Math.cos(elapsedTime) * 2.2 + Math.cos(elapsedTime) * 2.5;
 
   const ghost3Angle = -elapsedTime * 0.18;
   ghost3.position.x =
     Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
   ghost3.position.z =
     Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
-  ghost3.position.y = Math.cos(elapsedTime) * 5 + Math.cos(elapsedTime) * 2;
+  ghost3.position.y = Math.cos(elapsedTime) * 3 + Math.cos(elapsedTime) * 2.2;
 
   // Update controls
   controls.update();
